0c8f6aab7d7a4005cfbc004d2c11caf935bde8a0
Florian Klampfer <mail@qwtel.com>
2021-03-08 09:07:51 +0700

    Add throw for disallowed key

--- index.ts
+++ index.ts
@@ -189,3 +189,40 @@ const valueToKey = (input: AllowedKey, seen: Set<object> = new Set()): InternalA
     throw new Error();
   }
 };
+
+/**
+ * Copyright 2019 Google Inc.
+ * Licensed under Apache-2.0.
+ * <https://github.com/GoogleChromeLabs/kv-storage-polyfill>
+ */
+ export function throwForDisallowedKey(key: any) {
+  if (!isAllowedAsAKey(key)) {
+    throw Error('kv-storage: The given value is not allowed as a key: ' + key);
+  }
+}
+
+function isAllowedAsAKey(value: any) {
+  if (typeof value === 'number' || typeof value === 'string') {
+    return true;
+  }
+
+  if (typeof value === 'object' && value) {
+    if (Array.isArray(value)) {
+      return true;
+    }
+
+    if (value instanceof Date) {
+      return true;
+    }
+
+    if (ArrayBuffer.isView(value)) {
+      return true;
+    }
+
+    if (value instanceof ArrayBuffer) {
+      return true;
+    }
+  }
+
+  return false;
+}
